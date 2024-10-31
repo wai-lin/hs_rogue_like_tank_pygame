5a350cd76e03bd8ea33c40fcbe9f82171925e486
Wai Lin Aung <wailinaung@Wais-MacBook-Pro.local>
2024-10-31 13:29:58 +0700

    feat: bot tank random movement

--- game.py
+++ game.py
@@ -3,9 +3,11 @@ Tank game
 """
 
 import math
+from os import curdir
+import random
+from typing import List
 import pygame
 
-from typing import List
 
 # game config
 GAME = {
@@ -92,21 +94,33 @@ TILE = {
 }
 
 MAP = [
-    ["grass_1", "grass_2", "grass_road_SplitE", "grass_road_East", "grass_road_East", "grass_road_East", "grass_road_TransitionE", "sand_road_CornerLL", "sand_1", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE", "sand_road_CornerUR", "sand_road_CornerLL", "sand_2", "sand_1", "sand_2", "sand_road_SplitE", "sand_road_East", "sand_road_East"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE", "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE", "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
-    ["grass_road_East", "grass_road_East", "grass_road_Crossing", "grass_road_East", "grass_road_CornerLL", "grass_2", "grass_transitionE", "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2", "grass_transitionE", "sand_2", "sand_road_CornerUR", "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_CornerUL", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2", "grass_transitionE", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2", "grass_transitionE", "sand_2", "sand_1", "sand_2", "sand_road_CornerLR", "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_East"],
-    ["grass_1", "grass_2", "grass_road_CornerUR", "grass_road_East", "grass_road_SplitN", "grass_road_East", "grass_road_TransitionE", "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_CornerUL", "sand_2", "sand_1", "sand_2", "sand_1"],
-    ["grass_1", "grass_2", "grass_1", "grass_2", "grass_1", "grass_2", "grass_transitionE", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_SplitE", "grass_road_East", "grass_road_East", "grass_road_East", "grass_road_TransitionE",
+        "sand_road_CornerLL", "sand_1", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE", "sand_road_CornerUR",
+        "sand_road_CornerLL", "sand_2", "sand_1", "sand_2", "sand_road_SplitE", "sand_road_East", "sand_road_East"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE",
+        "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_1", "grass_2", "grass_transitionE",
+        "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
+    ["grass_road_East", "grass_road_East", "grass_road_Crossing", "grass_road_East", "grass_road_CornerLL", "grass_2",
+        "grass_transitionE", "sand_2", "sand_road_North", "sand_2", "sand_1", "sand_2", "sand_road_North", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2", "grass_transitionE", "sand_2",
+        "sand_road_CornerUR", "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_CornerUL", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2",
+        "grass_transitionE", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_road_North", "grass_2", "grass_road_North", "grass_2", "grass_transitionE", "sand_2",
+        "sand_1", "sand_2", "sand_road_CornerLR", "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_East"],
+    ["grass_1", "grass_2", "grass_road_CornerUR", "grass_road_East", "grass_road_SplitN", "grass_road_East", "grass_road_TransitionE",
+        "sand_road_East", "sand_road_East", "sand_road_East", "sand_road_CornerUL", "sand_2", "sand_1", "sand_2", "sand_1"],
+    ["grass_1", "grass_2", "grass_1", "grass_2", "grass_1", "grass_2", "grass_transitionE",
+        "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1", "sand_2", "sand_1"],
 ]
 
+
 def draw_map(screen: pygame.Surface) -> None:
-    for row in range(len(MAP)):
-        for col in range(len(MAP[row])):
+    """Drawing map on screen."""
+    for row, _ in enumerate(MAP):
+        for col, _ in enumerate(MAP[row]):
             tile_type = MAP[row][col]
             tile_asset = TILES[tile_type]
             img = pygame.image.load(f"assets/imgs/tiles/{tile_asset}")
@@ -117,7 +131,9 @@ def draw_map(screen: pygame.Surface) -> None:
 
 
 class Bullet:
-    def __init__(self, x: float, y: float, direction: str,  asset: str=BULLET["asset"]) -> None:
+    """Bullet object."""
+
+    def __init__(self, x: float, y: float, direction: str,  asset: str = BULLET["asset"]) -> None:
         self.x = x
         self.y = y
         self.speed_x = 0
@@ -126,7 +142,8 @@ class Bullet:
 
         self.img = pygame.image.load(asset)
         self.img = pygame.transform.scale(self.img, BULLET["size"])
-        self.img = pygame.transform.rotate(self.img, BULLET_DIRECTION[direction])
+        self.img = pygame.transform.rotate(
+            self.img, BULLET_DIRECTION[direction])
 
         if direction == "UP":
             self.speed_y = -6
@@ -138,25 +155,29 @@ class Bullet:
             self.speed_x = -6
 
     def move(self) -> None:
+        """Move bullet to x and y."""
         self.y += self.speed_y
         self.x += self.speed_x
 
     def draw(self, screen: pygame.Surface) -> None:
+        """Draw the bullet according to x and y."""
         screen.blit(self.img, (self.x, self.y))
 
 
 class Tank:
+    """Tank object."""
+
     def __init__(
-            self,
-            x: float,
-            y: float,
-            bullet: type[Bullet]=Bullet,
-            reload_time: int=500,
-            asset: str=TANK["asset"],
-            death_asset: str=TANK["asset"],
-            health: int=6,
-            bullet_asset: str=BULLET["asset"],
-        ) -> None:
+        self,
+        x: float,
+        y: float,
+        bullet: type[Bullet] = Bullet,
+        reload_time: int = 500,
+        asset: str = TANK["asset"],
+        death_asset: str = TANK["asset"],
+        health: int = 6,
+        bullet_asset: str = BULLET["asset"],
+    ) -> None:
         self.x = x
         self.y = y
         self.speed = 2
@@ -193,7 +214,7 @@ class Tank:
         # initial movement variables
         move_x, move_y = 0, 0
 
-        if self.initial_direction == None:
+        if self.initial_direction is None:
             if keys[pygame.K_LEFT]:
                 self.initial_direction = "LEFT"
             if keys[pygame.K_RIGHT]:
@@ -277,8 +298,8 @@ class Tank:
         if self.can_shoot():
             bullet_center_x = self.x
             bullet_center_y = self.y
-            x_shift = 17 # (half_of_tank - half_of_bullet) = (21 - 4)
-            y_shift = 13 # (half_of_tank - half_of_bullet) = (23 - 10)
+            x_shift = 17  # (half_of_tank - half_of_bullet) = (21 - 4)
+            y_shift = 13  # (half_of_tank - half_of_bullet) = (23 - 10)
 
             if self.direction == "UP":
                 bullet_center_x = self.x + x_shift
@@ -293,7 +314,8 @@ class Tank:
                 bullet_center_x = self.x + (3 * x_shift)
                 bullet_center_y = self.y + (1.4 * y_shift)
 
-            bullet = self.bullet(bullet_center_x, bullet_center_y, self.direction, asset=self.bullet_asset)
+            bullet = self.bullet(
+                bullet_center_x, bullet_center_y, self.direction, asset=self.bullet_asset)
             self.bullets.append(bullet)
             self.last_shot_time = pygame.time.get_ticks()
 
@@ -304,13 +326,15 @@ class Tank:
     def check_bullet_collision(self, tanks_list: List["Tank"]) -> None:
         """Check if the bullets hit any other tanks."""
         for bullet in self.bullets[:]:
-            bullet_rect = pygame.Rect(bullet.x, bullet.y, bullet.img.get_width(), bullet.img.get_height())
+            bullet_rect = pygame.Rect(
+                bullet.x, bullet.y, bullet.img.get_width(), bullet.img.get_height())
 
             for tank in tanks_list:
                 if tank == self:
                     continue
 
-                tank_rect = pygame.Rect(tank.x, tank.y, tank.rect.width, tank.rect.height)
+                tank_rect = pygame.Rect(
+                    tank.x, tank.y, tank.rect.width, tank.rect.height)
 
                 if bullet_rect.colliderect(tank_rect):
                     self.bullets.remove(bullet)
@@ -318,8 +342,10 @@ class Tank:
                     if tank.health <= 0:
                         tank.is_alive = False
                         tank.img = pygame.image.load(tank.death_asset)
-                        tank.img = pygame.transform.scale(tank.img, TANK["size"])
-                        tank.img = pygame.transform.rotate(tank.img, TANK_DIRECTION[tank.direction])
+                        tank.img = pygame.transform.scale(
+                            tank.img, TANK["size"])
+                        tank.img = pygame.transform.rotate(
+                            tank.img, TANK_DIRECTION[tank.direction])
                     break
 
     def draw_health_bar(self, screen: pygame.Surface) -> None:
@@ -327,15 +353,18 @@ class Tank:
         bar_width = 8
         bar_height = 8
         spacing = 2
-        bar_x = self.x - (self.rect.width // 2) + (3 * (bar_width + spacing)) // 2  # center the bar above the tank
+        # center the bar above the tank
+        bar_x = self.x - (self.rect.width // 2) + \
+            (3 * (bar_width + spacing)) // 2
         bar_y = self.y - 20  # position above the tank
 
-        fill_color = (239, 68, 68) # red
-        outline_color = (15, 23, 42) # dark blue
+        fill_color = (239, 68, 68)  # red
+        outline_color = (15, 23, 42)  # dark blue
 
         # draw the health boxes
         for i in range(self.health):
-            box_rect = (bar_x + i * (bar_width + spacing), bar_y, bar_width, bar_height)
+            box_rect = (bar_x + i * (bar_width + spacing),
+                        bar_y, bar_width, bar_height)
             # health box
             pygame.draw.rect(screen, fill_color, box_rect)
             # health outline box
@@ -344,7 +373,8 @@ class Tank:
     def draw(self, screen: pygame.Surface) -> None:
         """Paint the Tank and Bullets"""
         # draw the tank according to direction
-        rotated_img = pygame.transform.rotate(self.img, TANK_DIRECTION[self.direction])
+        rotated_img = pygame.transform.rotate(
+            self.img, TANK_DIRECTION[self.direction])
         center = (self.x + self.rect.width / 2, self.y + self.rect.height / 2)
         rotated_rect = rotated_img.get_rect(center=center)
         screen.blit(rotated_img, rotated_rect.topleft)
@@ -359,6 +389,47 @@ class Tank:
                 bullet.move()
                 bullet.draw(screen)
 
+class BotTank(Tank):
+    """BotTank object."""
+
+    def __init__(
+                self,
+                x: float,
+                y: float,
+                bullet: type[Bullet] = Bullet,
+                reload_time: int = 500,
+                asset: str = TANK["asset"],
+                death_asset: str = TANK["asset"],
+                health: int = 6,
+                bullet_asset: str = BULLET["asset"],
+                movement_interval: int = 2_000
+            ) -> None:
+        super().__init__(
+                x,
+                y,
+                bullet,
+                asset=asset,
+                reload_time=reload_time,
+                death_asset=death_asset,
+                health=health,
+                bullet_asset=bullet_asset
+            )
+        self.movement_interval = movement_interval
+        self.last_move_time = pygame.time.get_ticks()
+        self.current_direction = self.get_new_direction()
+
+    def get_new_direction(self) -> str:
+        """Get random direction"""
+        return random.choice(["UP", "DOWN", "LEFT", "RIGHT"])
+
+    def update_movement(self):
+        """Change new tank movement, according to movement interval."""
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_move_time >= self.movement_interval:
+            self.current_direction = self.get_new_direction()
+            self.last_move_time = current_time
+        self.move(self.current_direction)
+
 
 # setup the game
 pygame.init()
@@ -367,38 +438,43 @@ clock = pygame.time.Clock()
 
 # objects
 player = Tank(
-        x=300,
-        y=200,
-        asset="assets/imgs/tanks/tank_green.png",
-        death_asset="assets/imgs/tanks/tank_green_body.png",
-        bullet_asset="assets/imgs/tanks/bullet_green.png",
-    )
-enemy_1 = Tank(
-        x=100,
-        y=50,
-        asset="assets/imgs/tanks/tank_red.png",
-        death_asset="assets/imgs/tanks/tank_red_body.png",
-        bullet_asset="assets/imgs/tanks/bullet_red.png",
-    )
-enemy_2 = Tank(
-        x=200,
-        y=50,
-        asset="assets/imgs/tanks/tank_blue.png",
-        death_asset="assets/imgs/tanks/tank_blue_body.png",
-        bullet_asset="assets/imgs/tanks/bullet_blue.png",
-    )
-enemy_3 = Tank(
-        x=300,
-        y=50,
-        asset="assets/imgs/tanks/tank_sand.png",
-        death_asset="assets/imgs/tanks/tank_sand_body.png",
-        bullet_asset="assets/imgs/tanks/bullet_sand.png",
-    )
+    x=300,
+    y=200,
+    asset="assets/imgs/tanks/tank_green.png",
+    death_asset="assets/imgs/tanks/tank_green_body.png",
+    bullet_asset="assets/imgs/tanks/bullet_green.png",
+)
+bot_1 = BotTank(
+    x=100,
+    y=50,
+    asset="assets/imgs/tanks/tank_red.png",
+    death_asset="assets/imgs/tanks/tank_red_body.png",
+    bullet_asset="assets/imgs/tanks/bullet_red.png",
+    movement_interval=2_000,
+)
+bot_2 = BotTank(
+    x=200,
+    y=50,
+    asset="assets/imgs/tanks/tank_blue.png",
+    death_asset="assets/imgs/tanks/tank_blue_body.png",
+    bullet_asset="assets/imgs/tanks/bullet_blue.png",
+    movement_interval=3_000,
+)
+bot_3 = BotTank(
+    x=300,
+    y=50,
+    asset="assets/imgs/tanks/tank_sand.png",
+    death_asset="assets/imgs/tanks/tank_sand_body.png",
+    bullet_asset="assets/imgs/tanks/bullet_sand.png",
+    movement_interval=2_500,
+)
 
-all_tanks = [enemy_1, enemy_2, player, enemy_3]
+all_tanks = [bot_1, bot_2, bot_3, player]
+
 
 def sort_alive_tanks_on_last(t: "Tank"):
     return t.is_alive
+
 
 # game loop
 RUNNING = True
@@ -427,8 +503,11 @@ while RUNNING:
     draw_map(screen)
 
     # draw the tank and bullets
-    all_tanks.sort(key=sort_alive_tanks_on_last) # show the alive tanks on top of the dead tanks
+    # show the alive tanks on top of the dead tanks
+    all_tanks.sort(key=sort_alive_tanks_on_last)
     for tank in all_tanks:
+        if isinstance(tank, BotTank):
+            tank.update_movement()
         tank.draw(screen)
 
     """
@@ -441,4 +520,3 @@ while RUNNING:
 
 # exit game
 pygame.quit()
-
